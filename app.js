70c4d6813aeba258b6a2f8192e2fd975837a21b5
verbanick <110443737+verbanick@users.noreply.github.com>
2023-05-16 11:43:02 -0700

    Added port and mongoose

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
+const port = 3000;
+const mongoose = require('mongoose');
 
 const app = express();
 
@@ -10,3 +12,64 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({
     extended:true
 }));
+
+main().catch(err => console.log(err));
+
+async function main() {
+    await mongoose.connect('mongodb://127.0.0.1:27017/userDB');
+}
+
+const userSchema = {
+    email: String,
+    password: String
+}
+
+const User = new mongoose.model("User", userSchema);
+
+app.get("/", (req, res) => {
+    res.render("home");
+})
+
+app.get("/login", (req, res) => {
+    res.render("login");
+})
+
+////////////////////////////Register Page Routing ///////////////////////////////
+
+app.get("/register", (req, res) => {
+        res.render("register");
+    });
+
+app.post("/register", (req,res) => {
+        const newUser = new User({
+            email: req.body.username,
+            password: req.body.password
+        });
+
+        newUser.save()
+        .then(function(){
+            res.render("secrets");
+        })
+        .catch(function(err){
+            console.log(err);
+        })
+});
+
+app.post("/login", (req,res) =>{
+    const username = req.body.username;
+    const password = req.body.password;
+
+    User.findOne({email: username})
+        .then(function(foundUser){
+            if (foundUser.password === password) {
+                res.render("secrets");
+            }
+        })
+        .catch(function(err) {
+            console.log(err);
+        })
+})
+
+app.listen(port, () => {
+    console.log('Example app listening on port ${port}')
+})
