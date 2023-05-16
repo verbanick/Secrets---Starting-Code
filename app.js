64c2a082fe4227eb6ebfbcdaa80f984afc9b16c8
verbanick <110443737+verbanick@users.noreply.github.com>
2023-05-16 11:42:30 -0700

    Added Javascript

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 //jshint esversion:6
+const express = require('express');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+
+const app = express();
+
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({
+    extended:true
+}));
